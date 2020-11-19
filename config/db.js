18d4952fbe3812ed777ff01ed9f7267d696a0675
ruanfelipe7 <felipealmeida903@gmail.com>
2020-11-19 11:27:03 -0300

    initial commit

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const { mongo } = require("mongoose");
 
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://ruan_felipe07:<sitedalanchoneteapppassword0728>@cluster0.p4eke.mongodb.net/<lanchonete_app>?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://ruan_felipe07:sitedalanchoneteapppassword0728@cluster0.p4eke.mongodb.net/lanchonete_app?retryWrites=true&w=majority"}
 }else{
     module.exports = {mongoURI: "mongodb://localhost/lanchoneteAPP"}
 }
