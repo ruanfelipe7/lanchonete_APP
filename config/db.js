955c928903a1b9eb21b9ec07bf2356110dc44d37
Ruan Felipe <felipealmeida903@gmail.com>
2022-05-05 17:07:34 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,3 @@
 const { mongo } = require("mongoose");
-
-//if(process.env.NODE_ENV == "production"){
-    //module.exports = {mongoURI: "mongodb+srv://ruan_felipe07:sitedalanchoneteapppassword0728@cluster0.p4eke.mongodb.net/lanchoneteAPP?retryWrites=true&w=majority"}
-//}else{
     module.exports = {mongoURI: "mongodb://localhost/lanchoneteAPP"}
 //}
