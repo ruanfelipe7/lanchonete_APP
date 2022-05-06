39de139401f58ad8ee557a93d460a15791fd8c7f
ruanfelipe7 <felipealmeida903@gmail.com>
2022-05-05 22:08:29 -0300

    update: update menu

--- config/db.js
+++ config/db.js
@@ -1,2 +1,2 @@
 const { mongo } = require("mongoose");
-module.exports = {mongoURI: process.env.MONGODB_URI}    
+module.exports = {mongoURI: process.env.MONGODB_URI || 'mongodb+srv://ruanfelipe7:Jhq5RYw1UDkd7YAB@appblogapp.csg6j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'}    
